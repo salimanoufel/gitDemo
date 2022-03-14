139dd13c75be9e379316cd9c2453ed33e34f0bca
Salima Noufel <snoufel@SMB.SQLI.COM>
2022-03-14 16:06:07 +0100

    new change

--- Animal.java
+++ Animal.java
@@ -3,6 +3,10 @@ class Animal{
     public static String getDefaultGreeting(){
         return "Hello Animal";
     }
+
+    public static String getDBirdGreeting(){
+        return "Trr";
+    }
     public static void main (String[] args)
     {
         System.out.println(getDefaultGreeting());
