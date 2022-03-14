226919b64f35dd1d3729f98c6a52e582f5e016ed
Salima Noufel <snoufel@SMB.SQLI.COM>
2022-03-14 15:22:09 +0100

    Bird Implementation

--- Animal.java
+++ Animal.java
@@ -1,12 +1,13 @@
 class Animal{
-    public static String getDefaultGreeting(){
-       return"hello animal";
-    }
-    public static String getCatGreeting(){
-        return"Meow";
-     }
-    public static void main(String[] args ){
 
-        System.out.println(getBirdGreeting());
+    public static String getDefaultGreeting(){
+        return "Hello Animal";
+    }
+    public static String getDBirdGreeting(){
+        return "Trr";
+    }
+    public static void main (String[] args)
+    {
+        System.out.println(getDefaultGreeting());
     }
 }
