c31b2482885b3073e3e88a3f026e0695069663eb
Salima Noufel <snoufel@SMB.SQLI.COM>
2022-03-14 14:34:48 +0100

    Implemented Animal Greeting

---
+++ Animal.java
@@ -0,0 +1,9 @@
+class Animal{
+    public static String getDefaultGreeting(){
+       return"hello animal";
+    }
+    public static void main(String[] args ){
+
+        System.out.println(getDefaultGreeting());
+    }
+}
